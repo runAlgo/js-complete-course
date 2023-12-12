e281f164e27d9b2a747a4929b68d4e4109db586e
runAlgo <150834861+runAlgo@users.noreply.github.com>
2023-12-12 14:40:35 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const userId = 13545
+let userEmail = "programmer@google.com"
+var userPass = 12344
+userState = "Kanchanpur, Nepal"
+
+// userId = 333444 // not able 
+
+userEmail = "pro@microsoft.com"
+userPass = 455654
+userState = "KTM, Nepal"
+userGoal = "Coding"
+
+console.table([userId, userEmail, userPass, userGoal, userState])
